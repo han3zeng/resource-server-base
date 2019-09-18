51b34b6f3dff5549414d111ff578331a2de6ac81
Han <allen0346@gmail.com>
2019-09-18 11:55:55 +0800

    Adjust: connect mongodb server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-'use strict'
-
+'use strict';
 
 /* config */
 require('dotenv').config();
@@ -13,13 +12,24 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(jsonParser);
 app.use(urlencodedParser);
 
+/* connect to mongodb */
+const mongoose = require('mongoose');
+// keepAlive prevent TCP connection from closing because of idle status.
+const connectOptions = {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+};
+mongoose.connect(process.env.MONGO_URI, connectOptions, (err, db) => {
+  if (err) {
+    console.log('have problem connecting to mongoDb: ', err);
+  }
+  console.log('connect to mongoDb successfullly');
+});
 
 /* route */
 const routeHello = require('../routes/hello');
 routeHello(app);
 
-
-
 app.listen(process.env.PORT, () => {
-  console.log(`app is running on port: ${process.env.PORT}`)
-})
+  console.log(`app is running on port: ${process.env.PORT}`);
+});
