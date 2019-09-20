951ca6b68ba904743497f26e209ee5c28c66ef51
Han <allen0346@gmail.com>
2019-09-20 15:38:41 +0800

    Adjust: reconstruct the structure of project to fit testing env

--- server/server.js
+++ server/server.js
@@ -1,40 +1,15 @@
 'use strict';
 
-/* config */
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-// create application/json parser
-const jsonParser = bodyParser.json();
-// create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.use(jsonParser);
-app.use(urlencodedParser);
-
 /* connect to mongodb */
-const mongoose = require('mongoose');
-// keepAlive prevent TCP connection from closing because of idle status.
-const connectOptions = {
-  keepAlive: true,
-  reconnectTries: Number.MAX_VALUE
-};
-mongoose.connect(process.env.MONGO_URI, connectOptions, (err, db) => {
-  if (err) {
-    console.log('have problem connecting to mongoDb: ', err);
-  }
-  console.log('connect to mongoDb successfullly');
-});
+const db = require('../db/index.js');
+const app = require('../app/index.js');
 
-/* main */
-require('../models/UrlShorten');
-
-/* route */
-const routeHello = require('../routes/hello');
-const urlShorten = require('../routes/urlShorten');
-routeHello(app);
-urlShorten(app);
-
-app.listen(process.env.PORT, () => {
-  console.log(`app is running on port: ${process.env.PORT}`);
-});
+db.connect()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`app is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('mongodb non-catch error: ', err);
+  });
