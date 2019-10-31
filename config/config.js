8a22fea5c89048c52ef85f7c67101af79d573204
Han <allen0346@gmail.com>
2019-10-31 13:17:02 +0800

    Bug: NODE_ENV syntax + update version to 1-0-8

--- config/config.js
+++ config/config.js
@@ -3,5 +3,4 @@ const ENV_CONSTANTS = require('../constants/env');
 module.exports = Object.freeze({
   nodeEnv: process.NODE_ENV || ENV_CONSTANTS.DEVELOPMENT,
   origin: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'https://url-shortener-back.appspot.com' : 'http://localhost:8080'
-
 });
