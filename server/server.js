d7b99d9e8bc42575e01e101f98030d599e3d9a6e
Han <allen0346@gmail.com>
2019-11-15 09:26:11 +0800

    Adjust: default port 8080

--- server/server.js
+++ server/server.js
@@ -4,9 +4,11 @@
 const db = require('../db/index.js');
 const app = require('../app/index.js');
 
+const thePort = process.env.PORT || 8080;
+
 db.connect()
   .then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(thePort, () => {
       console.log(`app is running on port: ${process.env.PORT}`);
     });
   })
