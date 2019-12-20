e878876613d0b40623156176b5e62f21a398effd
Han <allen0346@gmail.com>
2019-12-20 14:53:35 +0800

    Adjust: config

--- config/config.js
+++ config/config.js
@@ -2,6 +2,10 @@ const ENV_CONSTANTS = require('../constants/env');
 
 module.exports = Object.freeze({
   nodeEnv: process.NODE_ENV || ENV_CONSTANTS.DEVELOPMENT,
-  origin: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'https://url-shortener-back.appspot.com' : 'http://localhost:8080',
-  allowedOrigins: ['http://localhost:3000', 'http://localhost:8080', 'https://portfolio-and-trial.appspot.com']
+  nodeEnvIsProd: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION,
+  origin: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'https://url-shortener-back.appspot.com' : 'http://localhost:3033',
+  domain: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? '.url-shortener-back.appspot.com' : 'localhost',
+  allowedOrigins: ['http://localhost:3000', 'https://portfolio-and-trial.appspot.com'],
+  authServerOrigin: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'https://authorization-server.appspot.com' : 'https://localhost:3030',
+  authServerHostName: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'authorization-server.appspot.com' : 'localhost:3030'
 });
