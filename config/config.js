7a24cf03677d8515c7388b6430a7945cf7a4ebaf
Han <allen0346@gmail.com>
2020-01-30 11:03:58 +0800

    Bug: provide correct origin of auth-server

--- config/config.js
+++ config/config.js
@@ -6,6 +6,6 @@ module.exports = Object.freeze({
   origin: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'https://url-shortener-back.appspot.com' : 'http://localhost:3033',
   domain: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? '.url-shortener-back.appspot.com' : 'localhost',
   allowedOrigins: ['http://localhost:3000', 'https://portfolio-and-trial.appspot.com'],
-  authServerOrigin: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'https://authorization-server.appspot.com' : 'https://localhost:3030',
+  authServerOrigin: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'https://authorization-server-261201.appspot.com' : 'https://localhost:3030',
   authServerHostName: process.NODE_ENV === ENV_CONSTANTS.PRODUCTION ? 'authorization-server.appspot.com' : 'localhost:3030'
 });
